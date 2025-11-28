5f8e9e68307006ab151e8174544081c42275b81f
esoncin5 <esoncin5@gmail.com>
2025-11-28 15:18:57 -0300

    arreglando titulo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -64,7 +64,7 @@ export default function Header() {
               className="rounded-full group-hover:scale-110 transition-transform"
             />
             <span className="text-2xl font-playfair font-bold text-[#FF5C8D]">
-              Porto Dulce
+              PortoDulce
             </span>
           </Link>
 
